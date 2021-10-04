86158bac949fbce7c0e653b3268523bee69e6547
Kahlo <martionlee@gmail.com>
2021-10-04 12:41:33 +0900

    Init tailwind css

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,6 +10,24 @@ export default function about() {
         <p>asdfasdfasdf</p>
         <p>asdfasdfasdf</p>
       </div>
+      <div
+        className="p-6
+        max-w-sm
+        mx-auto
+        bg-white
+        rounded-xl
+        shadow-md
+        flex
+        items-center
+        space-x-4"
+      >
+        <p className="text-gray-900">Tailwind test</p>
+
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
     </Layout>
   )
 }

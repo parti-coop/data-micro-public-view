a9323b6f0e32f84d829dca175bc6c7e8c07966cf
Seonghwa <martionlee@gmail.com>
2021-12-26 22:48:15 +0900

    파이 차트 값 표현 형식을 개선합니다

--- src/components/charts/MyPieChart.js
+++ src/components/charts/MyPieChart.js
@@ -28,22 +28,56 @@ const renderCustomizedLabel = ({
   outerRadius,
   percent,
   index,
+  fill,
+  value,
   name,
 }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+  const radius = innerRadius + (outerRadius - innerRadius) * 1.2
   const x = cx + radius * Math.cos(-midAngle * RADIAN)
   const y = cy + radius * Math.sin(-midAngle * RADIAN)
+  const sin = Math.sin(-RADIAN * midAngle)
+  const cos = Math.cos(-RADIAN * midAngle)
+  const sx = cx + (outerRadius - 6) * cos
+  const sy = cy + (outerRadius - 6) * sin
+  const mx = cx + (outerRadius + 15) * cos
+  const my = cy + (outerRadius + 15) * sin
+  const ex = mx + (cos >= 0 ? 1 : -1) * 10
+  const ey = my
+  const textAnchor = cos >= 0 ? 'start' : 'end'
 
   return (
-    <text
-      x={x}
-      y={y}
-      fill="black"
-      textAnchor={x > cx ? 'start' : 'end'}
-      dominantBaseline="central"
-    >
-      {`${name} ${(percent * 100).toFixed(0)}%`}
-    </text>
+    <g>
+      {/* <text
+        x={x}
+        y={y}
+        fill="black"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${name} (${(percent * 100).toFixed(0)}%)`}
+      </text> */}
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+        stroke-width="2"
+      />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 7}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`${name}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 7}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`(${(percent * 100).toFixed(0)}%)`}
+      </text>
+    </g>
   )
 }
 
@@ -67,7 +101,7 @@ export default function MyPieChart({ data, columns }) {
             dataKey={columns[1]}
             cx="50%"
             cy="50%"
-            outerRadius={(ref?.current?.offsetWidth - 10) / 2 ?? 180}
+            outerRadius={(ref?.current?.offsetWidth - 50) / 2 ?? 180}
             // fill={palette[0]}
             label={renderCustomizedLabel}
             labelLine={false}
